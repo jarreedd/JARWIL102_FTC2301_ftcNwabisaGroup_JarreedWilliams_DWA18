ef5dd93d33436eff23f3bab0f60ca43f0aff67ee
jarreed <jarreedw@gmail.com>
2023-07-27 12:37:05 +0200

    rotate menu button

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,15 +1,34 @@
 import React from "react";
 
 const Menu = () => {
+    const [showMore, setShowMore] = React.useState(false);
+
+    function toggleMenu() {
+        setShowMore((showMore) => !showMore);
+        console.log(showMore);
+    }
+
     return (
-        <>
-            <div className="menu-btn">
-                <div className="btn-line"></div>
-                <div className="btn-line"></div>
-                <div className="btn-line"></div>
-            </div>
-        </>
+        <div
+            onClick={toggleMenu}
+            className={showMore ? "menu-btn" : "menu-btn close"}
+        >
+            <div className="btn-line"></div>
+            <div className="btn-line"></div>
+            <div className="btn-line"></div>
+        </div>
     );
 };
 
 export default Menu;
+
+// if (!showMore) {
+//     menuBtn.classList.add("close");
+
+//     // Set Menu State
+//     showMore = true;
+// } else {
+//     menuBtn.classList.remove("close");
+//     // Set Menu State
+//     showMore = false;
+// }
