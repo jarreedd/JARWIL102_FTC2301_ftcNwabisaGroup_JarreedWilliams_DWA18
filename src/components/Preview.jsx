72b331f5999e96face1032e92cc9cde6d0011061
jarreed <jarreedw@gmail.com>
2023-07-27 10:16:32 +0200

    added preview details

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 
+const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+];
+
 const Preview = ({ data }) => {
-    return <div className="preview"></div>;
+    const date = new Date(data.updated);
+    const day = date.getDate();
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+    const updatedDate = `${day} ${month} ${year}`;
+
+    return (
+        <div className="preview">
+            <img src={data.image} />
+
+            <div className="preview--details">
+                <p>seasons: {data.seasons}</p>
+                <p>last update: {updatedDate}</p>
+                <p>genre: </p>
+            </div>
+        </div>
+    );
 };
 
 export default Preview;
