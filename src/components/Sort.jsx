844ca7ec43ee71c3bca7a758623f08bc89293ad9
jarreed <jarreedw@gmail.com>
2023-07-27 11:32:57 +0200

    change color palette

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -5,7 +5,7 @@ const Sort = ({ title }) => {
         <div className="browse--filters">
             <form className="filter--form">
                 <h3>{title}</h3>
-                <button>All</button>
+                <button className="active">All</button>
                 <button>A-Z</button>
                 <button>Z-A</button>
                 <button>Most Recant</button>
