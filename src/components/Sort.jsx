63e27faa092e21da47ca5c7f1551ad2eede6895a
jarreed <jarreedw@gmail.com>
2023-07-30 17:37:33 +0200

    sort buttons

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,15 +1,77 @@
 import React from "react";
 
 const Sort = ({ title }) => {
+    const [sort, setSort] = React.useState("all");
+
+    function formHandler(event) {
+        event.preventDefault();
+    }
+
+    function activeSort(event) {
+        setSort(event.target.name);
+    }
+
     return (
         <div className="browse--filters">
-            <form className="filter--form">
+            <form className="filter--form" onSubmit={formHandler}>
                 <h3>{title}</h3>
-                <button className="active">All</button>
-                <button>A-Z</button>
-                <button>Z-A</button>
-                <button>Most Recant</button>
-                <button>Least Recant</button>
+                <div className="sort-btn-container">
+                    <div className="left-arrow">
+                        <ion-icon name="chevron-back-outline"></ion-icon>
+                    </div>
+                    <div className="sort-list" onClick={activeSort}>
+                        <button
+                            className={
+                                sort === "all" ? "sort-btn active" : "sort-btn"
+                            }
+                            name="all"
+                        >
+                            All
+                        </button>
+                        <button
+                            className={
+                                sort === "a-z" ? "sort-btn active" : "sort-btn"
+                            }
+                            name="a-z"
+                        >
+                            A-Z
+                        </button>
+                        <button
+                            className={
+                                sort === "z-a" ? "sort-btn active" : "sort-btn"
+                            }
+                            name="z-a"
+                        >
+                            Z-A
+                        </button>
+                        <button
+                            className={
+                                sort === "most-recent"
+                                    ? "sort-btn active"
+                                    : "sort-btn"
+                            }
+                            name="most-recent"
+                        >
+                            Most Recent
+                        </button>
+                        <button
+                            className={
+                                sort === "least-recent"
+                                    ? "sort-btn active"
+                                    : "sort-btn"
+                            }
+                            name="least-recent"
+                        >
+                            Least Recent
+                        </button>
+                    </div>
+                    <div className="right-arrow active">
+                        <ion-icon name="chevron-forward-outline"></ion-icon>
+                    </div>
+                </div>
+                <button type="button" className="filters--btn">
+                    filters
+                </button>
             </form>
         </div>
     );
