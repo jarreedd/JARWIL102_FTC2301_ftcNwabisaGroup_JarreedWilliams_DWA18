c2e2d913bb9687a1d784327ef17e21c40f3b38d4
jarreedd <jarreedw@gmail.com>
2023-09-16 00:28:07 +0200

    connected supabase Auth

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import supabase from "../../config/supabaseClient";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
 
 const Login = () => {
-    return <div>Login</div>;
+    return (
+        <Auth
+            supabaseClient={supabase}
+            appearance={{ theme: ThemeSupa }}
+            providers={["google", "facebook"]}
+        />
+    );
 };
 
 export default Login;
